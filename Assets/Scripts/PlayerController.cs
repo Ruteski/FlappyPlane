0624ca7c61a44101be582c512dd7153ad755d6b6
Lincoln Ruteski <lruteski@gmail.com>
2022-03-29 14:50:11 -0300

    pegando o RB do player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //pegando as informações do player
+    private Rigidbody2D meuRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //pegando meu RB sozinho
+        meuRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
