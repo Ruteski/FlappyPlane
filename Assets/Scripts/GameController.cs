56f859b78f91a447a2e7996d1926c5a494e65495
Lincoln Ruteski <lruteski@gmail.com>
2022-03-29 20:53:16 -0300

    alterando a frequencia da criacao dos obstaculos

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
     //timer
     [SerializeField] private float timer = 1f;
     [SerializeField] private GameObject obstaculo;
@@ -12,24 +11,26 @@ public class GameController : MonoBehaviour
     [SerializeField] private float posMin = -0.3f;
     [SerializeField] private float posMax = 2.4f;
 
+    //tempo min e max para criacao dos obstaculos
+    [SerializeField] private float tMin = 1f;
+    [SerializeField] private float tMax = 4f;
+
     //posicao para criar obstaculo
     [SerializeField] private Vector3 posicao;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         timer -= Time.deltaTime;
 
         if (timer <= 0f) {
-            timer = 1f;
+            timer = Random.Range(tMin, tMax);
 
-            posicao.y = Random.Range(posMin, posMax);   
+            posicao.y = Random.Range(posMin, posMax);
 
             //criando os obstaculos
             Instantiate(obstaculo, posicao, Quaternion.identity); // ou da pra usar o tranform.rotation no lugar do quaternion
