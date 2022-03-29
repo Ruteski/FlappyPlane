8924dde107644439f4426e64f43905aec35227b8
Lincoln Ruteski <lruteski@gmail.com>
2022-03-29 15:16:50 -0300

    refatorando o codigo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,12 +19,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Subir();
+
+        LimitandoVelocidadeQueda();
+    }
+
+    void Subir() {
         //subindo ao apertar o espaço
         if (Input.GetKeyDown(KeyCode.Space)) {
             //fazendo a velocidade do RB ir para cima
             meuRB.velocity = Vector2.up * velocidade;
         }
+    }
 
+    void LimitandoVelocidadeQueda() {
         //limitando a velocidade de queda
         if (meuRB.velocity.y < -velocidade) {
             //travando a velocidade de queda em -5
